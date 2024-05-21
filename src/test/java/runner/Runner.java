c93e39b546f649b2dc036fc870f3aaf94ecbe29c
jyothsna <jessysree11@gmail.com>
2024-05-21 10:22:47 +0200

    first commit

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,17 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features ={ "src/test/java/features"},
+        glue = {"stepDefs"},
+        tags = "@Test3",
+        dryRun = false,
+        plugin = {"pretty", "html:target/cucumber-html-report.html"})
+public class Runner {
+
+}
